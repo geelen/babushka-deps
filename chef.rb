11f96174745defef3d4d45e781301a15bab3b23d
Glen Maddern <glenmaddern@gmail.com>
2010-04-13 11:05:54 +1000

    pseudocode dep

--- chef.rb
+++ chef.rb
@@ -1,7 +1,29 @@
 def 'chef installed' do
   requires 'chef', 'ohai'
   
-  #
+  # this needs to go somewhere, like ~/solo.rb
+  solo_rb = %Q{
+    file_cache_path "/tmp/chef-solo"
+    cookbook_path "/tmp/chef-solo/cookbooks"
+    recipe_url "http://s3.amazonaws.com/chef-solo/bootstrap-latest.tar.gz"
+  }
+  
+  # somewhere like ~/chef.json
+  chef_json = %Q{
+    {
+      "bootstrap": {
+        "chef": {
+          "url_type": "http",
+          "init_style": "runit",
+          "path": "/srv/chef",
+          "serve_path": "/srv/chef",
+          "server_fqdn": "#{server_name or `hostname -f`}",
+          "webui_enabled": true
+        }
+      },
+      "run_list": [ "recipe[bootstrap::server]" ]
+    }
+  }
 end
 
 gem 'chef', 'ohai'

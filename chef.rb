efd927d728a601d85684d8635796a6e8dddeeceb
Glen Maddern <glenmaddern@gmail.com>
2010-04-13 11:54:15 +1000

    fleshed out with ben's help

--- chef.rb
+++ chef.rb
@@ -1,53 +1,38 @@
-dep 'chef installed' do
-  requires 'chef', 'ohai'
-  
-  meet {
-    # render these two files
-    
-    # this needs to go somewhere, like ~/solo.rb
-    solo_rb = %Q{
-      file_cache_path "/tmp/chef-solo"
-      cookbook_path "/tmp/chef-solo/cookbooks"
-      recipe_url "http://s3.amazonaws.com/chef-solo/bootstrap-latest.tar.gz"
-    }
-  
-    # render this somewhere like ~/chef.json
-    chef_json = %Q{
-      {
-        "bootstrap": {
-          "chef": {
-            "url_type": "http",
-            "init_style": "runit",
-            "path": "/srv/chef",
-            "serve_path": "/srv/chef",
-            "server_fqdn": "#{var :server_name}",
-            "webui_enabled": true
-          }
-        },
-        "run_list": [ "recipe[bootstrap::server]" ]
-      }
-    }
-    
-    #on my manual deployment, one of these steps failed because apt-get update hadn't been run :/ Can I script that here?
-
-    `sudo chef-solo -c ~/solo.rb -j ~/chef.json`
-
-    #then I want to install nginx and use this proxy to access the chef server config.
-    #I don't understand the username file though. Chef has its own login. Do I need this?
-
-    nginx_proxy = %Q{
-      location /chef_admin {
-        auth_basic "Restricted";
-        auth_basic_user_file /etc/nginx/haproxy.users;
-
-        proxy_pass http://localhost:4040/;
-      }
-    }
-    
-  }
-  
-  
+dep 'chef' do
+  requires 'chef gem', 'ohai', 'chef solo', 'chef vhost enabled'
 end
 
-gem 'chef'
+dep 'chef solo' do
+  requires 'chef solo config files'
+  met? { dunno }
+  meet { sudo "chef-solo -c ~/solo.rb -j ~/chef.json" }
+end
+
+dep 'chef solo config files' do
+  helper :files do
+    %w[solo.rb chef.json]
+  end
+  met? { files.all? {|file| babushka_config? "~/#{file}" } }
+  meet { files.each {|file| render_erb "chef/#{file}.erb", :to => "~/#{file}" } }
+end
+
+nginx 'chef vhost enabled' do
+  requires 'vhost configured'
+  set :chef_vhost_link, (var(:nginx_prefix) / "conf/vhosts/on/chef_admin.conf")
+  met? { var(:chef_vhost_link).exists? }
+  meet { sudo "ln -sf '#{var(:chef_vhost_conf)}' '#{var(:chef_vhost_link)}'" }
+  after { restart_nginx }
+end
+
+nginx 'chef vhost configured' do
+  requires 'webserver configured'
+  set :chef_vhost_conf, (var(:nginx_prefix) / "conf/vhosts/chef_admin.conf")
+  met? { var(:chef_vhost_conf).exists? }
+  meet { render_erb "chef/chef_admin_vhost.conf.erb", :to => var(:chef_vhost_conf), :sudo => true }
+  after { restart_nginx if var(:chef_vhost_link).exists? }
+end
+
+gem 'chef gem' do
+  installs 'chef'
+end
 gem 'ohai'

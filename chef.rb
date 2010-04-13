cd6533074c65018cbe66f34031de9af0b39df01d
Glen Maddern <glenmaddern@gmail.com>
2010-04-13 11:15:29 +1000

    more pseudocode

--- chef.rb
+++ chef.rb
@@ -1,5 +1,6 @@
 def 'chef installed' do
   requires 'chef', 'ohai'
+  define_var :server_name, :default => shell('hostname -f')
   
   # this needs to go somewhere, like ~/solo.rb
   solo_rb = %Q{
@@ -8,7 +9,7 @@ def 'chef installed' do
     recipe_url "http://s3.amazonaws.com/chef-solo/bootstrap-latest.tar.gz"
   }
   
-  # somewhere like ~/chef.json
+  # render this somewhere like ~/chef.json
   chef_json = %Q{
     {
       "bootstrap": {
@@ -17,13 +18,29 @@ def 'chef installed' do
           "init_style": "runit",
           "path": "/srv/chef",
           "serve_path": "/srv/chef",
-          "server_fqdn": "#{server_name or `hostname -f`}",
+          "server_fqdn": "#{var :server_name}",
           "webui_enabled": true
         }
       },
       "run_list": [ "recipe[bootstrap::server]" ]
     }
   }
+  
+  #on my manual deployment, one of these steps failed because apt-get update hadn't been run :/ Can I script that here?
+  
+  `sudo chef-solo -c ~/solo.rb -j ~/chef.json`
+  
+  #then I want to install nginx and use this proxy to access the chef server config. Not that I exactly understand what I want this to do :)
+  
+  nginx_proxy = %Q{
+    location /chef_admin {
+      auth_basic "Restricted";
+      auth_basic_user_file /etc/nginx/haproxy.users;
+
+      proxy_pass http://localhost:4040/;
+    }
+  }
 end
 
-gem 'chef', 'ohai'
+gem 'chef'
+gem 'ohai'

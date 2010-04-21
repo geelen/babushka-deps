02fa6458fd8685046039439c8b17db542b481bb6
Glen Maddern <glenmaddern@gmail.com>
2010-04-21 13:54:53 +1000

    oops, should have used a helper

--- rails.rb
+++ rails.rb
@@ -1,5 +1,7 @@
 dep 'rails app db yaml present' do
-  db_yaml = (var(:rails_root) / "config" / "database.yml")
+  helper :db_yaml do
+    (var(:rails_root) / "config" / "database.yml")
+  end
   met? { db_yaml.exists? }
   meet { shell "cp #{var(:rails_root) / "config" / "database.yml.#{var(:rails_env)}"} #{db_yaml}" }
 end

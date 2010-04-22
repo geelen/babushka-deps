da952f6e73cede81909c94e82559b43d1b31319e
Glen Maddern <glenmaddern@gmail.com>
2010-04-22 16:28:36 +1000

    added dep 'switch babushka install to fork' to let me use my fork in production

--- babushka.rb
+++ babushka.rb
@@ -9,3 +9,29 @@ dep 'reverse babushka sources order' do
     reversed = true
   }
 end
+
+dep 'switch babushka install to fork' do
+
+  define_var :branch_name, :message => "Which branch to reset to?", :default => "master"
+  helper :fork_at_origin do
+    shell("git remote -v")[/origin\W*git:\/\/github\.com\/(\w+)\/babushka\.git/, 1]
+  end
+  met? {
+    in_dir(Babushka::Path.prefix / 'babushka') {
+      current_fork = fork_at_origin
+      define_var :fork_name, :message => "Whose fork do you want to be on?", :default => current_fork
+      log "Currently on branch #{current_fork}"
+      current_fork == var(:fork_name)
+    }
+  }
+  meet {
+    in_dir(Babushka::Path.prefix / 'babushka') {
+      current_fork = fork_at_origin
+      shell("git remote rm #{current_fork}") #this will fail nicely if it isn't there
+      shell("git remote rename origin #{current_fork}")
+      shell("git remote add origin git://github.com/#{var(:fork_name)}/babushka.git")
+      shell("git fetch origin")
+      shell("git reset --hard origin/#{var(:branch_name)}")
+    }
+  }
+end

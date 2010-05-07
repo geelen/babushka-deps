17320979bc69b561c9981c652008620f9f8fc52a
Glen Maddern <glenmaddern@gmail.com>
2010-05-07 10:11:36 +1000

    now works for database.staging.yml and database.yml.staging

--- rails.rb
+++ rails.rb
@@ -3,5 +3,5 @@ dep 'rails app db yaml present' do
     (var(:rails_root) / "config" / "database.yml")
   end
   met? { db_yaml.exists? }
-  meet { shell "cp #{var(:rails_root) / "config" / "database.yml.#{var(:rails_env)}"} #{db_yaml}" }
+  meet { shell "cp #{var(:rails_root) / "config" / "database.*#{var(:rails_env)}*"} #{db_yaml}" }
 end

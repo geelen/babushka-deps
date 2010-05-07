b33aefc5f6e40f5500c8c96f2156577cc49fe7b2
Glen Maddern <glenmaddern@gmail.com>
2010-05-07 10:25:34 +1000

    added more vars, and a hpricot dep

--- envato.rb
+++ envato.rb
@@ -2,11 +2,20 @@ gem 'right_aws' do
   provides []
 end
 
+gem 'hpricot' do
+  provides []
+end
+
 dep 'marketplace configured' do
   requires 'right_aws', 'rails app'
   setup {
     set :username, 'app'
     set :nginx_prefix, '/opt/nginx'
+    set :domain, 'staging-ec2.activeden.net'
+    set :passenger_repo_root, '~/current'
+    set :rails_root, '~/current'
+    set :rails_env, 'staging'
+    set :db, 'mysql'
   }
 end
 

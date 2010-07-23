bc68417c15b12ed5d5c0d7232bbcd9d6399421f1
Glen Maddern <glenmaddern@gmail.com>
2010-07-23 11:35:36 +1000

    stubbing sphinx

--- sphinx.rb
+++ sphinx.rb
@@ -2,3 +2,9 @@ dep 'sphinx.src' do
   source "http://www.sphinxsearch.com/downloads/sphinx-0.9.8.tar.gz"
   provides 'search', 'searchd', 'indexer'
 end
+
+dep 'sphinx running' do
+  requires 'sphinx.src'
+#  met? {}
+#  meet {}
+end

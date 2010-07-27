4a0391fef473e114e3123bc3f8c972e63421c77a
Glen Maddern <glenmaddern@gmail.com>
2010-07-27 16:21:29 +1000

    these deps need root

--- util.rb
+++ util.rb
@@ -1,26 +1,33 @@
+dep 'running as root' do
+  met? { shell "whomai" == 'root' }
+end
+
 dep 'monit', :template => 'managed'
 
 dep 'monit running' do
-  requires 'monit', 'monit startable'
+  requires 'monit'
+  requires_when_unmet 'monit startable'
   met? { (sudo "monit status")[/uptime/] }
   meet { sudo "/etc/init.d/monit start" }
 end
 
-dep 'monitrc configured' do
-  define_var :monit_frequency, :default => 30
-  define_var :monit_port, :default => 9111
-  met? { babushka_config? "/etc/monit/monitrc" }
-  meet { render_erb "monit/monitrc.erb", :to => "/etc/monit/monitrc" }
-  after { sudo "chmod 700 /etc/monit/monitrc" }
-end
-
 dep 'monit startable' do
-  requires 'monitrc configured', 'monit config is where we expect'
+  requires 'running as root', 'monitrc configured', 'monit config is where we expect'
   met? { grep "startup=1", "/etc/default/monit" }
   meet { change_line "startup=0", "startup=1", "/etc/default/monit" }
 end
 
+dep 'monitrc configured' do
+  requires 'running as root'
+  define_var :monit_frequency, :default => 30
+  define_var :monit_port, :default => 9111
+  met? { babushka_config? "/etc/monit/monitrc" }
+  meet { render_erb "monit/monitrc.erb", :to => "/etc/monit/monitrc" }
+  after { shell "chmod 700 /etc/monit/monitrc" }
+end
+
 dep 'monit config is where we expect' do
+  requires 'running as root'
   met? { "/etc/default/monit".p.exists? }
-  meet { sudo "echo startup=0 >> /etc/default/monit" }
+  meet { shell "echo startup=0 >> /etc/default/monit" }
 end

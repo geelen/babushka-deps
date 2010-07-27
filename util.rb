b4562965f286c2f5b6152ef34d563cd1f15ce789
Glen Maddern <glenmaddern@gmail.com>
2010-07-27 15:51:19 +1000

    monitrc config maybe

--- util.rb
+++ util.rb
@@ -16,7 +16,12 @@ dep 'monit config is where we expect' do
   meet { sudo "echo startup=0 >> /etc/default/monit" }
 end
 
-dep 'monitrc configured'
+dep 'monitrc configured' do
+  define_var :monit_frequency, :default => 30
+  define_var :monit_port, :default => 9111
+  met? { babushka_config? "/etc/monit/monitrc" }
+  meet { render_erb "monit/monitrc.erb", :to => "/etc/monit/monitrc" }
+end
 
 dep 'monit configured' do
 

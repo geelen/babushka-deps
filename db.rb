8b283cb73b93989e6a13f2693fba8b2d6fdafb3e
Glen Maddern <glenmaddern@gmail.com>
2010-07-28 13:54:39 +1000

    need mysql!

--- db.rb
+++ db.rb
@@ -5,6 +5,7 @@ dep 'existing db' do
 end
 
 dep 'existing mysql db' do
+  requires 'benhoskings:mysql.managed'
   # figure out root passwords / users / etc later.
   met? { shell("echo 'SHOW DATABASES;' | mysql -u root").split("\n")[1..-1].any? {|l| /\b#{var :db_name}\b/ =~ l } }
   meet { shell "echo 'CREATE DATABASE #{var :db_name};' | mysql -u root" }

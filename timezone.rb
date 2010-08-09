670d6d2def4a9127cf001420cdf71cf77fd6e434
Glen Maddern <glenmaddern@gmail.com>
2010-08-09 10:44:31 +1000

    .

--- timezone.rb
+++ timezone.rb
@@ -2,7 +2,7 @@ dep 'time zone set' do
   met? { grep Regexp.new(Regexp.escape(var(:timezone, :default => "Australia/Melbourne"))), "/etc/timezone" }
   meet {
     if ('/usr/share/zoneinfo' / var(:timezone)).exists?
-      sudo "cat #{var(:timezone)} > /etc/timezone"
+      sudo "echo '#{var(:timezone)}' > /etc/timezone"
       sudo "dpkg-reconfigure --frontend noninteractive tzdata"
     else
       log_error "Timezone #{var(:timezone)} is not valid! Check what is in /usr/share/zoneinfo"

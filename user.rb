a890b24eaf42ea250cba4f96d5b2e932cec5989d
Glen Maddern <glenmaddern@gmail.com>
2010-08-17 17:48:40 +1000

    explicit uid and gids being used in account creation

--- user.rb
+++ user.rb
@@ -1,13 +1,6 @@
-# wish there was a better way to overwrite deps, but here we are
-
-dep 'user exists with password' do
-  requires 'benhoskings:user exists'
-  on :linux do
-    met? { grep(/^#{var(:username)}:[^\*!]/, '/etc/shadow') }
-    meet {
-      sudo "echo -e '#{var(:password)}\n#{var(:password)}' | passwd #{var(:username)}"
-    }
-  end
+dep 'user set up from root', :on => :linux do
+  setup { set :home_dir_base, "/home" }
+  requires 'user exists with password', 'authorized key present for user'
 end
 
 dep 'authorized key present for user' do
@@ -19,15 +12,30 @@ dep 'authorized key present for user' do
   after { sudo "chown -R #{var(:username)}:#{var(:username)} '#{ssh_dir}'; chmod 600 '#{ssh_dir}/authorized_keys'" }
 end
 
-dep 'user set up from root', :on => :linux do
-  setup { set :home_dir_base, "/home" }
-  requires 'user exists with password', 'authorized key present for user'
-end
-
 dep 'user setup' do
   requires 'benhoskings:user setup', 'bash-completion'
   setup {
     set :github_user, 'geelen'
     set :dot_files_repo, 'dot-files'
+  }
+end
+
+dep 'user exists with password' do
+  requires 'user exists'
+  on :linux do
+    met? { grep(/^#{var(:username)}:[^\*!]/, '/etc/shadow') }
+    meet {
+      sudo "echo -e '#{var(:password)}\n#{var(:password)}' | passwd #{var(:username)}"
+    }
+  end
+end
+
+dep 'user exists' do
+  met? { grep(/^#{var(:username)}:/, '/etc/passwd') }
+  meet {
+    sudo "mkdir -p #{var :home_dir_base}" and
+      sudo "groupadd -g #{var :fixed_uid_and_gid} #{var :username}" and
+      sudo "useradd -m -s /bin/bash -b #{var :home_dir_base} -G admin #{var(:username)} -u #{var :fixed_uid_and_gid}" and
+      sudo "chmod 701 #{var(:home_dir_base) / var(:username)}"
   }
 end

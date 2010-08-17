1dc25ef95f6de9092998a71a496d10a30d520297
Glen Maddern <glenmaddern@gmail.com>
2010-08-17 18:16:48 +1000

    .

--- cron.rb
+++ cron.rb
@@ -4,6 +4,6 @@ meta :crontab do
   template {
     helper(:crontab) { "/var/spool/cron/crontabs" / user.first }
     met? { lines_to_add.all? { |line| sudo("grep '#{line.to_s.chomp.strip}' #{crontab}") } }
-    meet { append_to_file lines.join("\n") + "\n\n", crontab, :sudo => !File.writable?(crontab) }
+    meet { append_to_file lines_to_add.join("\n") + "\n\n", crontab, :sudo => !File.writable?(crontab) }
   }
 end

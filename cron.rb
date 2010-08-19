b83003043bb12c2fe3720ecd0dd861ca0f8c18f5
Glen Maddern <glenmaddern@gmail.com>
2010-08-19 16:24:09 +1000

    might fix this, needs testing.

--- cron.rb
+++ cron.rb
@@ -3,8 +3,8 @@ meta :crontab do
   accepts_list_for :lines_to_add
   template {
     met? {
-      existing_crontab = shell("crontab -l")
-      lines_to_add.all? { |line| sudo("grep -e '#{Regexp.escape(line.to_s.chomp.strip)}' #{crontab}") }
+      existing_crontab = sudo "crontab -l", :as => user.first.to_s
+      lines_to_add.all? { |line| existing_crontab =~ Regexp.new(Regexp.escape(lines_to_add).gsub(/ +/," +")) }
     }
     meet { append_to_file lines_to_add.join("\n") + "\n\n", crontab, :sudo => !File.writable?(crontab) }
   }

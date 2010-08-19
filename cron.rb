2ee7bea029d402c855ff0fe82ebaca15b36b3c90
Glen Maddern <glenmaddern@gmail.com>
2010-08-19 16:24:19 +1000

    bugger it, do it later

--- cron.rb
+++ cron.rb
@@ -1,11 +1,11 @@
-meta :crontab do
-  accepts_list_for :user
-  accepts_list_for :lines_to_add
-  template {
-    met? {
-      existing_crontab = sudo "crontab -l", :as => user.first.to_s
-      lines_to_add.all? { |line| existing_crontab =~ Regexp.new(Regexp.escape(lines_to_add).gsub(/ +/," +")) }
-    }
-    meet { append_to_file lines_to_add.join("\n") + "\n\n", crontab, :sudo => !File.writable?(crontab) }
-  }
-end
+#meta :crontab do
+#  accepts_list_for :user
+#  accepts_list_for :lines_to_add
+#  template {
+#    met? {
+#      existing_crontab = sudo "crontab -l", :as => user.first.to_s
+#      lines_to_add.all? { |line| existing_crontab =~ Regexp.new(Regexp.escape(lines_to_add).gsub(/ +/," +")) }
+#    }
+#    meet { append_to_file lines_to_add.join("\n") + "\n\n", crontab, :sudo => !File.writable?(crontab) }
+#  }
+#end

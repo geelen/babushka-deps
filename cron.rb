803e2c95dd5224541de5c8e32f22518e5f58bf4e
Glen Maddern <glenmaddern@gmail.com>
2010-08-19 10:08:15 +1000

    not sure of the best way to do this

--- cron.rb
+++ cron.rb
@@ -2,8 +2,10 @@ meta :crontab do
   accepts_list_for :user
   accepts_list_for :lines_to_add
   template {
-    helper(:crontab) { "/var/spool/cron/crontabs" / user.first }
-    met? { lines_to_add.all? { |line| sudo("grep -e '#{Regexp.escape(line.to_s.chomp.strip)}' #{crontab}") } }
+    met? {
+      existing_crontab = shell("crontab -l")
+      lines_to_add.all? { |line| sudo("grep -e '#{Regexp.escape(line.to_s.chomp.strip)}' #{crontab}") }
+    }
     meet { append_to_file lines_to_add.join("\n") + "\n\n", crontab, :sudo => !File.writable?(crontab) }
   }
 end

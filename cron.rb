f7b98dbf08fc9ec20cb05e035e21face344d851c
Glen Maddern <glenmaddern@gmail.com>
2010-08-25 17:10:28 +1000

    added cron meta

--- cron.rb
+++ cron.rb
@@ -1,11 +1,18 @@
 meta :crontab do
-  accepts_list_for :user
   accepts_list_for :lines_to_add
-#  template {
-#    met? {
-#      existing_crontab = sudo "crontab -l", :as => user.first.to_s
-#      lines_to_add.all? { |line| existing_crontab =~ Regexp.new(Regexp.escape(lines_to_add).gsub(/ +/," +")) }
-#    }
-#    meet { append_to_file lines_to_add.join("\n") + "\n\n", crontab, :sudo => !File.writable?(crontab) }
-#  }
+#  accepts_list_for L{[
+#    ["* * * * *", var :command_one],
+#    ["*/5 * * * *", var :command_two],
+#  ]}
+  helper(:existing_crontab) { shell "crontab -l" }
+  template {
+    met? {
+      existing_crontab && lines_to_add.all? { |lines| lines.all? { |schedule, command|
+        existing_crontab[command]
+      }}
+    }
+    meet {
+      shell("crontab -", :input => lines_to_add.map { |lines| lines.map { |schedule, command| "#{schedule}   #{command}" }.join("\n") }.join("\n"))
+    }
+  }
 end

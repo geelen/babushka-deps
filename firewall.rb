f17daba9e44027927f6b13e1de578ac3e1bd9f2f
Glen Maddern <glenmaddern@gmail.com>
2010-09-06 16:39:29 +1000

    need to force ufw enable, otherwise it hangs on a prompt

--- firewall.rb
+++ firewall.rb
@@ -1,13 +1,13 @@
 dep 'firewall enabled' do
   requires_when_unmet 'firewall permits SSH'
   met? { sudo("ufw status")["Status: active"] }
-  meet { sudo "ufw enable" }
+  meet { sudo "ufw -f enable" }
 end
 
 dep 'firewall disabled' do
   requires_when_unmet 'firewall permits SSH'
   met? { sudo("ufw status")["Status: inactive"] }
-  meet { sudo "ufw disable" }
+  meet { sudo "ufw -f disable" }
 end
 
 dep 'firewall permits SSH' do

abf6fb12f827cacc19887335cedf0fdff9b48267
Glen Maddern <glenmaddern@gmail.com>
2010-09-29 15:35:47 +1000

    bundler optionally uses --local and --path vendor/bundler

--- rails.rb
+++ rails.rb
@@ -8,7 +8,7 @@ dep 'bundler installed' do
   requires 'bundler.gem', 'local gemdir writable'
   define_var :bundler_installed_locally, :default => 'n'
   met? { in_dir(var(:rails_root)) { shell "bundle check", :log => true } }
-  meet { in_dir(var(:rails_root)) { sudo "bundle install --without test,cucumber #{'--local' if var(:bundler_installed_locally).to_s =~ /^y/}", :log => true }}
+  meet { in_dir(var(:rails_root)) { sudo "bundle install --without test,cucumber #{'--local' if var(:bundle_local).to_s =~ /^y/} #{'--path vendor/bundle' if var(:bundle_into_vendor).to_s =~ /^y/}", :log => true }}
 end
 
 dep 'bundler.gem' do

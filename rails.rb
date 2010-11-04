1e6cc992b9a2394c8275f1670c213526034d9c3d
Glen Maddern <glenmaddern@gmail.com>
2010-11-04 15:02:39 +1100

    two top-level deps ported

--- rails.rb
+++ rails.rb
@@ -4,19 +4,6 @@ dep 'rails app db yaml present' do
   meet { shell "cp #{var(:rails_root) / "config" / "database.*#{var(:rails_env)}*"} #{db_yaml}" }
 end
 
-dep 'bundler installed' do
-  requires 'bundler.gem', 'local gemdir writable'
-  define_var :bundler_installed_locally, :default => 'n'
-  define_var :bundle_into_vendor, :default => 'n'
-  met? { in_dir(var(:rails_root)) { shell "bundle check", :log => true } }
-  meet { in_dir(var(:rails_root)) { sudo "bundle install --without test,cucumber #{'--local' if var(:bundle_local).to_s =~ /^y/} #{'--path vendor/bundle' if var(:bundle_into_vendor).to_s =~ /^y/}", :log => true }}
-end
-
-dep 'bundler.gem' do
-  installs 'bundler' => '1.0.0.rc.5'
-  provides 'bundle'
-end
-
 dep 'local gemdir writable' do
   helper(:local_path) { "~/.gem".p }
   met? { File.writable_real?(local_path) }

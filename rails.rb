08053e82ad569c6f7aae42de852542533d93653f
Glen Maddern <glenmaddern@gmail.com>
2010-11-05 15:10:50 +1100

    all top_level deps done!

--- rails.rb
+++ rails.rb
@@ -1,12 +1,3 @@
-dep 'local gemdir writable' do
-  helper(:local_path) { "~/.gem".p }
-  met? { File.writable_real?(local_path) }
-  meet {
-    sudo "mkdir -p #{local_path}"
-    sudo "chown #{var(:username)}:#{var(:username)} #{local_path}"
-  }
-end
-
 dep 'rails app' do
   requires 'webapp', 'benhoskings:passenger deploy repo', 'benhoskings:db gem', 'bundler installed', 'db set up'
   define_var :rails_env, :default => 'production'
@@ -24,12 +15,4 @@ end
 dep 'libxslt-dev.managed' do
   installs { via :apt, 'libxslt1-dev' }
   provides []
-end
-
-dep 'rails app chowned' do
-  met? { @rails_app_chowned_run }
-  meet {
-    sudo "chown -R #{var(:username)}:#{var(:username)} #{var(:rails_root)}"
-    @rails_app_chowned_run = true
-  }
 end

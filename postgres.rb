62442c69b7f8d557c611fe8cbcecba51475752ab
Glen Maddern <glenmaddern@gmail.com>
2011-08-17 14:31:23 +1000

    added stemming to my dictionary

--- postgres.rb
+++ postgres.rb
@@ -1,9 +1,10 @@
 # encoding: UTF-8
 
-dep 'postgres has a simple unaccenting dictionary' do
-  define_var :search_configuration_name, :default => 'simple_unaccenting'
+dep 'postgres has a unaccenting stemming dictionary' do
+  define_var :dictionary_name, :default => 'english_stemmer'
+  define_var :search_configuration_name, :default => 'unaccenting_english_stemmer'
   define_var :postgres_shared_path, :default => '/usr/share/postgresql/9.0'
-  requires 'unaccenting installed', 'text search configuration installed'
+  requires 'unaccenting installed', 'english stemming dictionary installed', 'text search configuration installed'
 end
 
 dep 'unaccenting installed' do
@@ -15,11 +16,6 @@ dep 'unaccenting installed' do
   }
 end
 
-dep 'postgresql-contrib.managed' do
-  requires 'benhoskings:postgres.managed'
-  provides []
-end
-
 dep 'unaccent files exist' do
   requires_when_unmet 'postgresql-contrib.managed'
   met? {
@@ -28,17 +24,33 @@ dep 'unaccent files exist' do
   }
 end
 
+dep 'postgresql-contrib.managed' do
+  requires 'benhoskings:postgres.managed'
+  provides []
+end
+
 dep 'interpunct is a dash' do
   met? { grep /•\t-/, var(:postgres_shared_path) / 'tsearch_data/unaccent.rules' }
   meet { sudo 'echo -e "•\t-" >> ' + var(:postgres_shared_path) / 'tsearch_data/unaccent.rules' }
+end
+
+dep 'english stemming dictionary installed' do
+  met? { shell("psql #{var(:db_name)} -c '\\dFd'") =~ /public.*#{var :dictionary_name}/ }
+  meet {
+    shell "psql #{var(:db_name)}", :input => %Q{
+CREATE TEXT SEARCH DICTIONARY public.#{var :dictionary_name} (TEMPLATE = pg_catalog.snowball, LANGUAGE = english);
+}
+    }
 end
 
 dep 'text search configuration installed' do
   met? { shell("psql #{var(:db_name)} -c '\\dF'") =~ /public.*#{var :search_configuration_name}/ }
   meet {
     shell "psql #{var(:db_name)}", :input => %Q{
-create text search configuration public.#{var :search_configuration_name} (copy = pg_catalog.simple);
-alter text search configuration #{var :search_configuration_name} alter mapping for word, hword, hword_part with unaccent, simple;
+create text search configuration public.#{var :search_configuration_name} (copy = pg_catalog.english);
+alter text search configuration #{var :search_configuration_name}
+  alter mapping for asciihword, asciiword, hword, hword_asciipart, hword_numpart, hword_part, word
+  with unaccent, #{var :dictionary_name};
 }
   }
 end
